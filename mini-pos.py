e158be86e5e762b7c8c500fec5060d5eedb88d45
okorojames <okorojameschizaram@gmail.com>
2023-01-03 11:42:50 +0100

    ci/cd

--- mini-pos.py
+++ mini-pos.py
@@ -1,3 +1,4 @@
+from time import sleep
 ussd = "*565#"
 numOne = "1"
 numTwo = "2"
@@ -10,6 +11,15 @@ else:
     putUssd = str(input(f"Please {yourName}, put \n Input The USSD: "))
     if putUssd == ussd:
         option = str(input(f"{yourName}, select from the following options: \n1: Deposit\n2: Withdraw\n3: Check Balance\n  "))
+        #
+        #
+        if option == numOne:
+            depoAmount = int(input(f"Please Enter Amount: ")) 
+            print('Please wait...')
+            sleep(5)
+            print(f'Transaction successful\nNew bal is: {balAmount + depoAmount}')
+        # 
+        # 
     elif putUssd == "":
         print(f"{yourName}, You can't submit and empty option.")
     else:
